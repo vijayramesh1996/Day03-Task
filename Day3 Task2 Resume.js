6c198591a972ef2096a01963c3f603b9f363503a
Vijay <113192642+vijayramesh1996@users.noreply.github.com>
2023-01-05 16:30:31 +0530

    Create Day3 Task2 Resume.js

---
+++ Day3 Task2 Resume.js
@@ -0,0 +1,43 @@
+let myResume=[{
+personaldetails : {
+      name: "Vijay.R",
+      email: "vijayjr1996@gmail.com",
+      phone: 8608750188,
+      degree: "B.E",
+      location: {
+        address: "Newperungalatur",
+        pinCode: 600063,
+        city: "Chennai",
+        state: "Tamilnadu",
+        country: "India"
+      }},
+    work: 
+      {
+        company: "R STAHL Private Limited (MNC)",
+        position: "ENGINEER-DESIGN",
+        startDate: "27-02-2019",
+        summary: "i am the design engineer for design the safety explosion protection area electrical panels(i.e, electrical panel installed in ships, coal mining, on-shore and off-shore places,etc) handling various countries customers and gaining lots of experience to handle the customer and complete the project till reasched on customer place with guide of installation",
+      },
+    education: 
+      {
+        institution: "Tagore college of engineering",
+        department: "ELECTRICAL AND ELECTRONICS ENGINEERING",
+        duration:2013-2017,
+      },
+   skills:{
+        name: "Full stack developer",
+        level: "beginner",
+      },
+    languages: 
+      {
+        language: "Tamil,English",
+      },
+    interests:
+      {
+        name: "To create a innovative things, learn new things and browsing the wikipedia,"},
+      
+    
+  }]
+
+  let myResume1 = JSON.stringify(myResume)
+  console.log(myResume1)
